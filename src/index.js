be0f71198caee31407e69866f59849cc32c79980
Burhonov <38163059+Burhonov@users.noreply.github.com>
2020-10-16 22:05:26 +0500

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const openingMatch = {};
+  for (const el of bracketsConfig) {
+      openingMatch[el[1]] = el[0];
+  }
+  const stack = [];
+  for (let i = 0; i < str.length; i++) {
+      if (openingMatch[str[i]] && openingMatch[str[i]] === stack[stack.length - 1]) {
+          stack.pop();
+      } else {
+          stack.push(str[i]);
+      }
+  }
+  return stack.length === 0;
 }
